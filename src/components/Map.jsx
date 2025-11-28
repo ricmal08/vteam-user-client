8f78dc42c4f573520ef9280665c74b3729d5b0e6
Jakob <jahl24@student.bth.se>
2025-11-28 03:48:58 +0100

    feat: fetch zones and render the polygon to the map in Stockholm

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,63 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Polygon } from 'react-leaflet'
+import { useEffect, useState } from "react";
 import styled from 'styled-components';
+
+// Color for the zone
+const greenOption = { color: 'green' };
 
 /*
 Renders a map using openstreetmap
 */
 function Map() {
+  // Array with zones for future with more zones
+  const [zones, setZones] = useState([]);
+
+  // Fetch from api
+  async function fetchZones() {
+    try {
+      const response = await fetch("http://localhost:3000/zones/stockholm");
+
+      console.log('response:', response.ok);
+
+      if (!response.ok) {
+        throw new Error("Kunde inte hämta zoner");
+      }
+
+      const zone = await response.json();
+      console.log('zone:', zone);
+      console.log('coordinates:', zone.area.coordinates);
+      console.log('coordinates[0]', zone.area.coordinates[0]);
+
+      setZones([zone]);
+
+    } catch (error) {
+      console.error("Error while fetching zones:", error);
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchZones();
+  }, []);
+
   return (
     <Wrapper>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={[59.3293, 18.0686]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+      {/* write the zone, need to flip the positions to lat long due to GeoJson sends long lat */}
+      {zones.map((zone) => (
+        <Polygon
+          key={zone._id}
+          // Map coordinates to be able to flip them, check console.log to see the array when fetching
+          positions={zone.area.coordinates[0].map(coord => [coord[1], coord[0]])}
+          // Set the color with the variabel
+          pathOptions={greenOption}
+        />
+
+      ))}
       </MapContainer>
     </Wrapper>
   )
